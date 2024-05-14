fed59b314cc3629898d0f5d694947099633f5911
benjamibono <benjamibono@gmail.com>
2024-05-14 12:19:14 +0200

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,5 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://benjamibono.github.io',
-  base: 'dental-barcelo-web',
   integrations: [tailwind()]
 });
