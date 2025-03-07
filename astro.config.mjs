bab899ecb343a3b308fb1ed8296083aa69aac84f
benjamibono <benjamibono@gmail.com>
2025-03-07 17:47:47 +0100

    fix styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
-import tailwindcss from "tailwindcss";
-import autoprefixer from "autoprefixer";
 
 // https://astro.build/config
 export default defineConfig({
@@ -19,33 +17,6 @@ export default defineConfig({
   image: {
     service: {
       entrypoint: "astro/assets/services/sharp",
-    },
-  },
-  vite: {
-    build: {
-      rollupOptions: {
-        output: {
-          assetFileNames: (assetInfo) => {
-            const info = assetInfo.name.split(".");
-            const ext = info[info.length - 1];
-            if (/\.(png|jpe?g|gif|svg|webp|ico)$/.test(assetInfo.name)) {
-              return `assets/images/[name]-[hash][extname]`;
-            }
-            if (/\.(woff2?|eot|ttf|otf)$/.test(assetInfo.name)) {
-              return `assets/fonts/[name]-[hash][extname]`;
-            }
-            if (/\.css$/.test(assetInfo.name)) {
-              return `assets/styles/[name]-[hash][extname]`;
-            }
-            return `assets/[name]-[hash][extname]`;
-          },
-        },
-      },
-    },
-    css: {
-      postcss: {
-        plugins: [tailwindcss, autoprefixer],
-      },
     },
   },
 });
