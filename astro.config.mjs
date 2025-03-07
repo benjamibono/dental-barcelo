0bd88551574129751148ca1c590736ee46503d6e
benjamibono <benjamibono@gmail.com>
2025-03-07 17:45:50 +0100

    fixing styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
 
 // https://astro.build/config
 export default defineConfig({
@@ -32,9 +34,17 @@ export default defineConfig({
             if (/\.(woff2?|eot|ttf|otf)$/.test(assetInfo.name)) {
               return `assets/fonts/[name]-[hash][extname]`;
             }
+            if (/\.css$/.test(assetInfo.name)) {
+              return `assets/styles/[name]-[hash][extname]`;
+            }
             return `assets/[name]-[hash][extname]`;
           },
         },
+      },
+    },
+    css: {
+      postcss: {
+        plugins: [tailwindcss, autoprefixer],
       },
     },
   },
