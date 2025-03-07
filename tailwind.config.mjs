a2a9a40a0c7be3620f96a65a880cd9dcf27bfa1e
benjamibono <benjamibono@gmail.com>
2025-03-07 17:43:52 +0100

    fix styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,18 +1,25 @@
-import animations from '@midudev/tailwind-animations'
+import animations from "@midudev/tailwind-animations";
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-		screens: {
-			xs: '360px',
-			sm: '640px',
-			md: '768px',
-			lg: '1024px',
-			xl: '1280px',
-			xxl: '1536px',
-			xxxl: '1920px',
-		},
-	},
-	plugins: [animations],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "custom-dark": "#2e211c",
+        "custom-brown": "#574137",
+        "custom-accent": "#738ea0",
+        "custom-light": "#f1ece3",
+      },
+    },
+    screens: {
+      xs: "360px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      xxl: "1536px",
+      xxxl: "1920px",
+    },
+  },
+  plugins: [animations],
+};
